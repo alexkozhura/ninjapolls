235343533a8ce3532b3637013bfcb05500387f95
alexkozhura <alexkozhura@gmail.com>
2023-12-24 17:10:50 +0100

    Added CreatePollForm component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,27 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
 
 	// tabs
 	let items = ['Current Polls', 'Add New Poll'];
 	// also need to say which of them are going to be active
 	let activeItem = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 
 <Header />
 <main>
-	
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<p>Poll list component goes here</p>
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm />
+	{/if}
 </main>
 <Footer />
 
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,28 @@
+<script>
+    let fields = { question: '', answerA: '', answerB: '' };
+
+    const submitHander = () => {
+        console.log(fields);
+    }
+</script>
+
+<form on:submit|preventDefault={submitHander}>
+    <div class="form-field">
+        <label for="question">Poll Question:</label>
+        <input type="text" id="question" bind:value={fields.question}>
+    </div>
+    <div class="form-field">
+        <!-- We need the for field to link the label to the input -->
+        <label for="answer-a">Answer A:</label>
+        <input type="text" id="answer-a" bind:value={fields.answerA}>
+    </div>
+    <div class="form-field">
+        <label for="answer-b">Answer B:</label>
+        <input type="text" id="answer-b" bind:value={fields.answerB}>
+    </div>
+    <button>Add Poll</button>
+</form>
+
+<style>
+
+</style>
